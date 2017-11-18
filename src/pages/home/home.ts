f0d11f258ef0e0974cd6cad105813963d1db49dc
MohitsBranmac <ms24546@gmail.com>
2017-11-18 20:14:07 +0530

    adds a Provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private http: HttpClient
+  ) {
 
+  }
+
+  ionViewDidLoad () {
+    this.http.get ('/assets/data.json')
+      .subscribe (data => console.log(data))
   }
 
 }
